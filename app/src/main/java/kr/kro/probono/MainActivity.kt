fb05f041ab60144e7a5ca94c63e384cefbf393ff
root <root@debian>
2022-10-25 19:04:37 +0900

    Editting bluetooth functions

--- app/src/main/java/kr/kro/probono/MainActivity.kt
+++ app/src/main/java/kr/kro/probono/MainActivity.kt
@@ -1,59 +1,111 @@
 package kr.kro.probono
 
-import android.bluetooth.BluetoothDevice
-import android.bluetooth.BluetoothSocket
+import android.Manifest
 import android.content.Intent
-import android.os.Build
-import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.CheckBox
-import android.widget.EditText
-import androidx.appcompat.app.AppCompatActivity
 import com.skt.Tmap.TMapData
 import com.skt.Tmap.TMapTapi
 import com.skt.Tmap.TMapTapi.OnAuthenticationListenerCallback
-import kr.kro.probono.bluetooth.BlueModule
-import java.io.IOException
+import kr.kro.probono.R.*
+import android.os.Bundle
 import java.util.*
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.speech.tts.*
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import java.io.IOError
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var destName: EditText
-    private lateinit var exitOnArrive: CheckBox
-    private lateinit var exe: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val scrollChildLayout = R.layout.activity_main as ViewGroup;
-        scrollChildLayout.removeViewAt(R.layout.activity_main)
+        setContentView(layout.activity_main)
 
-        setContentView(findViewById(R.layout.activity_main))
-        exitOnArrive = findViewById(R.id.view_navigate_exit_on_arrive_CheckBox)
-        exe = findViewById(R.id.view_navigate_execute_button)
+
         val tapi = TMapTapi(this@MainActivity)
         tapi.setOnAuthenticationListener(object : OnAuthenticationListenerCallback {
             override fun SKTMapApikeySucceed() {
                 println("succeed api key")
-                println("installed : " + tapi.isTmapApplicationInstalled)
                 println(tapi.invokeTmap())
             }
 
             override fun SKTMapApikeyFailed(s: String) {
-                println("installed :!!!! " + tapi.isTmapApplicationInstalled)
             }
         })
         tapi.setSKTMapAuthentication("l7xx5330b9922ba74d55880d8ce5f1b0a568")
-        setContentView(exe);
-        exe.setOnClickListener(View.OnClickListener {
-            execute();
-            println("!!!!")
-        })
+
+        val exe: Button = findViewById(id.view_navigate_execute_button)
+        exe.setOnClickListener {
+            execute()
+            initListener()
+        }
     }
+
+    private fun initListener() {
+        val connPerm = arrayOf(
+            android.Manifest.permission.BLUETOOTH_CONNECT,
+        )
+        requestPermissions(connPerm, 100)
+           try {
+               var devLocals = BluetoothAdapter.getDefaultAdapter().getBondedDevices()
+               lateinit var dev : BluetoothDevice
+               val i=0;
+               for(i in devLocals) {
+                   if(i.name.equals("raspberrypi")) {
+                       dev=i;
+                       break
+                   } else if (i==devLocals.last()) {
+                       return
+                   }
+               }
+               val sock = dev.createInsecureRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
+                sock.run {
+                    try {
+                        this.connect()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    } catch (e: SecurityException) {
+                        e.printStackTrace()
+                    }
+                    while (true) {
+                        try {
+                            if(! sock.isConnected()) {
+                                sock.connect()
+                                continue
+                            }
+                            val outputStream = this!!.outputStream
+                            outputStream.write(1)
+                            val inputStream = this.inputStream
+                            var prev = ""
+                            var input = inputStream.readBytes().toString()
+                            if (input != prev) {
+                                prev = input;
+                                SynthesisRequest(input, Bundle())
+                                Log.d("Probono", input)
+                            }
+                        } catch (e: SecurityException) {
+                            e.printStackTrace()
+                        } catch (e:  IOException) {
+                            e.printStackTrace()
+                        }
+                    }
+                }
+            } catch (e: SecurityException) {
+                e.printStackTrace()
+            }
+    }
+
+
 
     private fun invokeNavigate(DestName: String?, isAutoClose: Boolean): Boolean {
 
         var szDestName  = DestName
-        if (szDestName != null && szDestName.toByteArray().size > 128) {
+        if ((szDestName != null) && (szDestName.toByteArray().size > 128)) {
             val byteTemp = ByteArray(128)
             System.arraycopy(szDestName.toByteArray(), 0, byteTemp, 0, 128)
             szDestName = String(byteTemp)
@@ -77,42 +129,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun execute() {
+        val destName :TextInputEditText = findViewById(id.view_navigate_destination_input)
+        val exitOnArrive :CheckBox = findViewById(id.view_navigate_exit_on_arrive_CheckBox)
         val x = 0f
         val y = 0f
         val id = 11
-        println(destName!!.text.toString() + " " + x + " " + y + " " + id + " " + exitOnArrive!!.isChecked)
+        println(destName.text.toString()+"로 도착지를 설정하셨습니다.")
         val b = invokeNavigate(
-                destName!!.text.toString(),
-                exitOnArrive!!.isChecked
+                destName.text.toString(),
+                exitOnArrive.isChecked
         )
         println(b)
     }
 
-    private inner class bgYolo : Runnable {
-        override fun run() {
-            try {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    socket = device!!.createInsecureRfcommSocketToServiceRecord(UUID.randomUUID())
-                }
-            } catch (e: SecurityException) {
-                e.printStackTrace()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-            if (device == null) {
-                println("device not found")
-                return
-            }
-            BlueModule.BlueConf.ConnectThread.__init__()
-            BlueModule.BlueConf.ConnectThread.__onRun__(findViewById(R.id.logView), socket)
-            println("device detected - " + device)
-        }
-    }
-
-
-
-    companion object {
-        lateinit var socket: BluetoothSocket
-        lateinit var device: BluetoothDevice
-    }
 }
